0848e64f0e1e78bffecf74ab6d98c41e2c8bd249
MateuszMarczuk <mateuszmarczuk95@gmail.com>
2018-06-18 00:20:41 +0200

    init

---
+++ src/main/java/pl/edu/wat/timlab/TimLabApp.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.timlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimLabApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimLabApp.class, args);
+	}
+
+}
---
+++ src/main/java/pl/edu/wat/timlab/service/RandomNumbersService.java
@@ -0,0 +1,8 @@
+package pl.edu.wat.timlab.service;
+
+import java.util.List;
+
+
+public interface RandomNumbersService {
+    List<Integer> getRandomNumbers(int amount);
+}
